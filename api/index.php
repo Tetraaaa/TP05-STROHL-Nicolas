60867e48629992bcbc16cca177fa15c4f518dbfb
Tetra <nicolas.strohl@gmail.com>
2021-03-30 14:43:57 +0200

    test jwt

--- api/index.php
+++ api/index.php
@@ -7,10 +7,30 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+const JWT_SECRET = "rendezmoimeschaussures";
+
+$options = [
+    "attribute" => "token",
+    "header" => "Authorization",
+    "regexp" => "/Bearer\s+(.*)$/i",
+    "secure" => false,
+    "algorithm" => ["HS256"],
+    "secret" => JWT_SECRET,
+    "path"=> ["/api"],
+    "ignore" => ["/api/login", "/api/register"],
+    "error" => function ($response, $arguments) {
+        $data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
+        $response = $response->withStatus(401);
+        return $response->withHeader("Content-Type",
+        "application/json")->getBody()->write(json_encode($data));
+    }
+];
+
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
     return $response;
 });
 
+$app->add(new Tuupola\Middleware\JwtAuthentication($options));
 $app->run();
