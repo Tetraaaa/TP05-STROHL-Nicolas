7b45116b9ec03cd030ae5ae66640963dd1271fb8
Tetra <nicolas.strohl@gmail.com>
2021-03-30 15:07:51 +0200

    Ajout dépendances manquantes

--- api/index.php
+++ api/index.php
@@ -2,6 +2,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Tuupola\Middleware\HttpBasicAuthentication;
+use \Firebase\JWT\JWT;
 
 require __DIR__ . '/../vendor/autoload.php';
 
