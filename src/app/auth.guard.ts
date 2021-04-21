41ee7cb616e93bba2ed29f89bc108ba095b30935
Tetra <nicolas.strohl@gmail.com>
2021-04-21 18:13:30 +0200

    Test ajout de magie noire sur le AuthGuard
    Le saviez-vous : 99% des tours de magie ne sont pas de la magie
    
    
    
    mais de la sorcellerie

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ApiHttpInterceptor } from './api-http-interceptor';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
-  }
-  
+
+    constructor(private apiHttpInterceptor:ApiHttpInterceptor) { }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+            return !!this.apiHttpInterceptor.jwtToken;
+    }
+
 }
