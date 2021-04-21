89ce6ff74b3fb8a251efca39bad3506739d3db8f
Tetra <nicolas.strohl@gmail.com>
2021-04-21 23:35:32 +0200

    Redirect au catalogue quand le login réussit

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormulaireService } from '../formulaire.service'
 @Component({
     selector: 'app-connexion',
@@ -11,14 +12,17 @@ export class ConnexionComponent implements OnInit {
     login: string = "";
     password: string = "";
 
-    constructor(private formulaireService: FormulaireService) { }
+    constructor(private formulaireService: FormulaireService, private router: Router) { }
 
     ngOnInit(): void {
     }
 
     connexion() {
         this.formulaireService.login(this.login, this.password).subscribe((flux) => {
-            console.log(JSON.stringify(flux));
+            if(!flux)
+            {
+                this.router.navigate(["/catalogue"]);
+            }
         });
     }
 }
