44c9df5938efbecfe0b65e0ea99f8a90562a417f
Tetra <nicolas.strohl@gmail.com>
2021-04-22 18:20:03 +0200

    Test localstorage

--- src/app/api-http-interceptor.ts
+++ src/app/api-http-interceptor.ts
@@ -6,24 +6,23 @@ import { Router } from '@angular/router'
 
 @Injectable()
 export class ApiHttpInterceptor implements HttpInterceptor {
-    jwtToken: String = "";
     constructor(private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (this.jwtToken != "") {
-            req = req.clone({ setHeaders: { Authorization: `Bearer ${this.jwtToken}` } });
+        if (localStorage.getItem("jwt")) {
+            req = req.clone({ setHeaders: { Authorization: `Bearer ${localStorage.getItem("jwt")}` } });
         }
 
         return next.handle(req).pipe(tap(
             (evt: HttpEvent<any>) => {
                 if (evt instanceof HttpResponse) {
 
-                    let tab: Array<String>;
+                    let tab: Array<string>;
                     let enteteAuthorization = evt.headers.get("Authorization");
                     if (enteteAuthorization != null) {
                         tab = enteteAuthorization.split(/Bearer\s+(.*)$/i);
                         if (tab.length > 1) {
-                            this.jwtToken = tab[1];
+                            localStorage.setItem("jwt", tab[1]);
                         }
                     }
 
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { ApiHttpInterceptor } from './api-http-interceptor';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-    constructor(private apiHttpInterceptor:ApiHttpInterceptor) { }
+    constructor(public router: Router) { }
 
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-            return !!this.apiHttpInterceptor.jwtToken;
+    canActivate():boolean
+    {
+        if(!!localStorage.getItem("jwt"))
+        {
+            return true;
+        }
+        else
+        {
+            this.router.navigate(["/"]);
+            return false;
+        }
     }
 
 }
