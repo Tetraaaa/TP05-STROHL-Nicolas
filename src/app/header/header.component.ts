400fcea27f1702159d08866d9172efc8cddcf9e3
Tetra <nicolas.strohl@gmail.com>
2021-04-22 19:24:28 +0200

    Le header ne s'affiche que quand on est login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,10 +14,12 @@ export class HeaderComponent implements OnInit {
   products$: Observable<Product[]>;
   productsLength:number;
   totalPrice:number;
+  isLoggedIn:boolean = false;
   constructor(private store:Store) { 
   }
 
   ngOnInit(): void {
+    this.isLoggedIn = !!localStorage.getItem("jwt");
     this.products$ = this.store.select(BasketState.getProducts);
     this.products$.subscribe((item) => {
       this.productsLength = item.length;
